31cff3bbe3944a279c6ed32a01e72692ed3091d9
米亚流年 <miyaliunian@gmail.com>
2024-01-15 10:57:57 +0800

    wefawef

---
+++ packages/switch/switch.vue
@@ -0,0 +1,18 @@
+<template>
+    <van-switch v-model="checked"  v-bind="$attrs"  v-on="$listeners"/>
+</template>
+<script>
+import { Switch } from 'vant';
+export default {
+     name:'d-switch',
+     components:{
+       [Switch.name]:Switch 
+     },
+      data() {
+        return {
+            checked: true,
+        };
+    },
+
+}
+</script>
