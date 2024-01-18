41018998add1029d6fc8688bb917f9c3ffaf763e
米亚流年 <miyaliunian@gmail.com>
2024-01-18 13:40:19 +0800

    feat: 测试依赖注入

--- packages/icon/icon.vue
+++ packages/icon/icon.vue
@@ -1,13 +1,33 @@
+<!--
+ * @Author: 米亚流年 miyaliunian@gmail.com
+ * @Date: 2024-01-17 14:51:53
+ * @LastEditors: 米亚流年 miyaliunian@gmail.com
+ * @LastEditTime: 2024-01-18 09:25:18
+ * @FilePath: /common-comp/packages/icon/icon.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
     <div style="display:flex;background-Color:red">
         <span>按钮祖航</span>
         <span class="iconfont iconiconfontyoujiantou-copy-copy-copy" style="font-size:200px"></span>
+        <div>依赖注入的内容</div>
+        <p style="background-color: aliceblue;color: #000;">:{{ this.parentName.value }}</p>
     </div>
 </template>
 <script>
  export default {
-     name: 'DIcon'
- }
+   name: 'DIcon',
+   inject: ['parentName'],
+   watch: {
+     parentName: {
+       handler: function (val, oldVal) {
+         console.log("val", val)
+         console.log("oldVal", oldVal)
+       },
+       immediate: true
+     }
+   },
+  }
 </script>
 <style> 
 @import './assets/fonts/iconfont.css'
--- script/build.js
+++ script/build.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 米亚流年 miyaliunian@gmail.com
+ * @Date: 2024-01-15 10:14:47
+ * @LastEditors: 米亚流年 miyaliunian@gmail.com
+ * @LastEditTime: 2024-01-17 18:17:12
+ * @FilePath: /common-comp/script/build.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const shell = require('shelljs');
 const path = require('path');
 let scripts = []
